cad13b831263d40bee3d675b92c335864b08b1d0
amesa007 <amesabarba@gmail.comm>
2025-02-08 14:04:54 -0500

    right pidgeon bus

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -29,7 +29,7 @@ public final class Constants {
     public static final class Swerve {
 
         public static final int pigeonID = 10;
-        public static final String pigeonCanBus = "DriveTrain"; //
+        public static final String pigeonCanBus = "rio"; //
         public static final boolean focEnabled = true; //TODO: This must be tuned to specific robot
         public static final boolean isOnCANivore = false;
 
