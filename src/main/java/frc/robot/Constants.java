823028346d938a1611386eca7bdfc137d3b676cf
Aidan Martzloff <martzloffkid3@gmail.com>
2025-02-08 13:54:19 -0500

    un-odom

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -29,9 +29,9 @@ public final class Constants {
     public static final class Swerve {
 
         public static final int pigeonID = 10;
-        public static final String pigeonCanBus = "Drivetrain"; //TODO: if on canivore, change this to CANivore name or serial number
+        public static final String pigeonCanBus = "rio";
 
-        public static final boolean focEnabled = true; //TODO: This must be tuned to specific robot
+        public static final boolean focEnabled = true;
         public static final boolean isOnCANivore = true;
 
         public static final COTSTalonFXSwerveConstants chosenModule =  //TODO: This must be tuned to specific robot
