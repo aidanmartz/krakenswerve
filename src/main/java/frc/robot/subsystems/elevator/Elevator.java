d7f9fbd10a7514b92902c53f9344e358d8ad6b12
Ben Bowers <nanobowers@gmail.com>
2025-02-17 15:27:12 -0500

    adding in attempt at a real elevator

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.elevator;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.units.measure.Distance;
@@ -55,6 +58,31 @@ public class Elevator extends SubsystemBase {
         actual.updateElevatorPosition(this.inputs.position);
         target.updateElevatorPosition(this.inputs.setpointPosition);
         goal.updateElevatorPosition(this.setpoint);
+    }
+
+    public enum ElevatorStop {
+        // Intake occurs at zero
+        SAFE,
+        L1,
+        L2,
+        L2_ALGAE,
+        L3,
+        L3_ALGAE,
+        L4
+    };
+
+    private final EnumMap<ElevatorStop, Double> elevatorHeights = new EnumMap<>(Map.ofEntries(
+            Map.entry(ElevatorStop.SAFE, 1.0),
+            Map.entry(ElevatorStop.L1, 6.0),
+            Map.entry(ElevatorStop.L2, 8.0),
+            Map.entry(ElevatorStop.L2_ALGAE, 13.0),
+            Map.entry(ElevatorStop.L3, 12.0),
+            Map.entry(ElevatorStop.L3_ALGAE, 16.0),
+            Map.entry(ElevatorStop.L4, 19.5)));
+
+
+    public Command moveTo(ElevatorStop stop) {
+        return Commands.runOnce(() -> this.setpoint = Inches.of(elevatorHeights.get(stop)));
     }
 
     public Command setPosition(Distance position) {
---
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOReal.java
@@ -0,0 +1,114 @@
+package frc.robot.subsystems.elevator;
+
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import frc.robot.Constants;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import edu.wpi.first.wpilibj2.command.Command;
+//import edu.wpi.first.wpilibj2.command.Commands;
+//import edu.wpi.first.wpilibj2.command.InstantCommand;
+//import edu.wpi.first.wpilibj2.command.RunCommand;
+//import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+//import edu.wpi.first.units.TemperatureUnit;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.MutDistance;
+import edu.wpi.first.units.measure.Voltage;
+
+import static edu.wpi.first.units.Units.*;
+
+public class ElevatorIOReal implements ElevatorIO {
+
+    private SparkFlex elevatorLeft;
+    private SparkFlex elevatorRight;
+    private SparkClosedLoopController closedLoopControllerLeft;
+    private SparkClosedLoopController closedLoopControllerRight;
+
+    //unused// private Stop nextStop = Stop.SAFE;
+    private MutDistance currentPosition = Inches.mutable(0.0);
+    private final ElevatorFeedforward el_Feedforward = new ElevatorFeedforward(1.0, 0.0, 0.0);
+
+    public ElevatorIOReal() {
+
+        elevatorLeft = setupElevatorSparkFlex(true, Constants.CANConstants.elevatorLeftId);
+        closedLoopControllerLeft = elevatorLeft.getClosedLoopController();
+        
+        elevatorRight = setupElevatorSparkFlex(false, Constants.CANConstants.elevatorRightId);
+        closedLoopControllerRight = elevatorRight.getClosedLoopController();
+    }
+
+    public SparkFlex setupElevatorSparkFlex(boolean left, int canid) {
+        SparkFlexConfig config = new SparkFlexConfig();
+        SparkFlex elevatorSpark = new SparkFlex(canid, MotorType.kBrushless);
+        config
+                .inverted(left) // left: inverted=true, right: inverted=false
+                .idleMode(IdleMode.kBrake);
+        config.encoder
+                .positionConversionFactor(1)
+                .velocityConversionFactor(1);
+        config.closedLoop
+                .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                .pid(0.1, 0.0, 0.0);
+        elevatorSpark.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        elevatorSpark.getEncoder().setPosition(0);
+        return elevatorSpark;
+    }
+
+    @Override
+    public void runSetpoint(Distance position) {
+        currentPosition.mut_replace(position); // probably unneeded since we are capturing inputs elsewhere?
+        double level = position.in(Inches);
+        double feedforward = el_Feedforward.calculate(1.0);
+        closedLoopControllerLeft.setReference(level, SparkFlex.ControlType.kPosition, ClosedLoopSlot.kSlot0,
+                feedforward);
+        closedLoopControllerRight.setReference(level, SparkFlex.ControlType.kPosition, ClosedLoopSlot.kSlot0,
+                feedforward);
+    }
+
+    @Override
+    public void updateInputs(ElevatorIOInputs inputs) {
+        //inputs.position.mut_replace(sim.getPositionMeters(), Meters); // ????
+        //inputs.velocity.mut_replace(sim.getVelocityMetersPerSecond(), MetersPerSecond); // ????
+        //inputs.setpointPosition.mut_replace(controller.getSetpoint(), Meters); // ????
+        //inputs.setpointVelocity.mut_replace(0, MetersPerSecond); // ????
+
+        inputs.position.mut_replace(elevatorLeft.getAbsoluteEncoder().getPosition(), Meters);
+        inputs.velocity.mut_replace(elevatorLeft.getAbsoluteEncoder().getVelocity(), MetersPerSecond);
+
+        inputs.appliedVoltsLeader.mut_replace(elevatorLeft.getBusVoltage(), Volts);
+        inputs.appliedVoltsFollower.mut_replace(elevatorRight.getBusVoltage(), Volts);
+
+        inputs.supplyCurrentLeader.mut_replace(elevatorLeft.getOutputCurrent(), Amps);
+        inputs.supplyCurrentFollower.mut_replace(elevatorRight.getOutputCurrent(), Amps);
+
+        inputs.torqueCurrentLeader.mut_replace(elevatorLeft.getOutputCurrent(), Amps);
+        inputs.torqueCurrentFollower.mut_replace(elevatorRight.getOutputCurrent(), Amps);
+
+        inputs.temperatureLeader.mut_replace(elevatorLeft.getMotorTemperature(), Celsius);
+        inputs.temperatureFollower.mut_replace(elevatorRight.getMotorTemperature(), Celsius);
+
+    }
+
+    public void runVolts(Voltage volts) {
+        elevatorLeft.setVoltage(volts);
+        elevatorRight.setVoltage(volts);
+    }
+/*
+    public void runCurrent(Current current) {}
+    public void setBrakeMode(boolean enabled) {}
+    public void setPID(double p, double i, double d) {}
+*/
+
+    public void stop() {
+        runVolts(Volts.of(0));
+    }
+
+}
